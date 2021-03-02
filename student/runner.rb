dee9e0fc4d7f1814a229f1e3aa0b7d02191486c8
Connie Hyman <connie2630a@gmail.com>
2021-03-02 00:48:22 -0800

    Refactor tests

---
+++ student/runner.rb
@@ -0,0 +1,9 @@
+require_relative 'student'
+
+test_student = Student.new("Jane", "Doe")
+
+puts test_student.first_name
+
+puts test_student.last_name
+
+puts test_student.fullname
--- student/student_spec.rb
+++ student/student_spec.rb
@@ -1,21 +1,21 @@
 require_relative 'student'
 
 RSpec.describe Student do
+
+  before do
+    @student = Student.new("John", "Doe")
+  end
  
   it 'should respond to #first_name' do
-    student = Student.new("John", "Doe")
-    expect(student).to respond_to(:first_name)
+    expect(@student).to respond_to(:first_name)
   end 
 
   it 'should respond to #last_name' do
-    student = Student.new("John", "Doe")
-    expect(student).to respond_to(:last_name)
-
+    expect(@student).to respond_to(:last_name)
   end
 
   it 'should respond to #student_fullname' do
-    student = Student.new("John", "Doe")
-    expect(student).to respond_to(#student_fullname)
+    expect(@student).to respond_to(:fullname)
   end
 
 end
